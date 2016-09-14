d1e97ceb091a58ade21794711c4fab1c7ca24404
hiep.vo <vanhiep1086@gmail.com>
2016-09-14 17:01:10 -0500

    remove CheckValidity()

--- Credit-Debit-Form/main.js
+++ Credit-Debit-Form/main.js
@@ -10,36 +10,35 @@ var errorMessage = document.getElementById('errorMessage');
 var errors       = [];
 
 form.addEventListener('submit', function(event){
-  if(!this.checkValidity()){
-    event.preventDefault();
+  event.preventDefault();
 
-    errors.splice(0, errors.length);
-    errorMessage.innerHTML = '';
-    if(validator.isEmpty(namecard.value)){
-      errors.push('Name on Card is required');
-    }
-
-    if(validator.isEmpty(card.value)){
-      errors.push('Card number is required');
-    }
-
-    if(validator.isEmpty(cvs.value)){
-      errors.push('The CVS number is required');
-    }
-    else if(!validator.isNumber(cvs) || cvs.length < 3){
-      errors.push('The CVS is not correct')
-    }
-
-    if(!validator.isCreditCard(card.value)){
-      errors.push('The Credit card number is not correct');
-    }
-
-    errors.forEach(function(error){
-      var li = document.createElement('li');
-      li.appendChild(document.createTextNode(error));
-      errorMessage.appendChild(li);
-    })
+  errors.splice(0, errors.length);
+  errorMessage.innerHTML = '';
+  if(validator.isEmpty(namecard.value)){
+    errors.push('Name on Card is required');
   }
+
+  if(validator.isEmpty(card.value)){
+    errors.push('Card number is required');
+  }
+
+  if(validator.isEmpty(cvs.value)){
+    errors.push('The CVS number is required');
+  }
+  else if(!validator.isNumber(cvs) || cvs.length < 3){
+    errors.push('The CVS is not correct')
+  }
+
+  if(!validator.isCreditCard(card.value)){
+    errors.push('The Credit card number is not correct');
+  }
+
+  errors.forEach(function(error){
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(error));
+    errorMessage.appendChild(li);
+  })
+
 }, false);
 
 // level 9,10,11
